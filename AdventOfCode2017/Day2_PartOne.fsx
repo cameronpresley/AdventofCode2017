c6c3f5c1834ee92003cb1721846d2e70a12928b0
Cameron Presley <Cameron@TheSoftwareMentor.com>
2017-12-02 21:58:53 -0500

    Saved files.

--- AdventOfCode2017/Day2_PartOne.fsx
+++ AdventOfCode2017/Day2_PartOne.fsx
@@ -1,1 +1,10 @@
-﻿
+﻿#load "Day2.fs"
+open Day2
+open System
+
+let lines = input
+                .Split([|"\n"|], System.StringSplitOptions.RemoveEmptyEntries) 
+                |> Array.map(fun s -> s.Split([|"\t"; " "; "\n"|], StringSplitOptions.RemoveEmptyEntries))
+                |> Array.map(fun s -> s |> Array.map(Int32.Parse))
+
+lines |> Array.fold(fun state line -> state + (line |> Array.max) - (line |> Array.min)) 0
--- AdventOfCode2017/Day2_PartTwo.fsx
+++ AdventOfCode2017/Day2_PartTwo.fsx
@@ -1,1 +1,19 @@
 ﻿
+#load "Day2.fs"
+open Day2
+open System
+
+let lines = input
+                .Split([|"\n"|], System.StringSplitOptions.RemoveEmptyEntries) 
+                |> Array.map(fun s -> s.Split([|"\t"; " "; "\n"|], StringSplitOptions.RemoveEmptyEntries))
+                |> Array.map(fun s -> s |> Array.map(Int32.Parse))
+
+let rec findDivisors nums =
+    match nums with
+    | [] -> 0
+    | h::t ->
+        match t |> List.tryFindBack(fun x -> x % h = 0) with
+        | Some x -> (x/h)
+        | None -> findDivisors t
+    
+lines |> Array.fold(fun state line -> state + (findDivisors(line|>Array.sort|>Array.toList))) 0
