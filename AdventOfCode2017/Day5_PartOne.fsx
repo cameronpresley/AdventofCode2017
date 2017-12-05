b7f6f166ca6108be2e4b47797e87698ac8acd917
Cameron Presley <Cameron@TheSoftwareMentor.com>
2017-12-05 06:42:37 -0500

    Added Day 5 Solution.

---
+++ AdventOfCode2017/Day5_PartOne.fsx
@@ -0,0 +1,18 @@
+﻿#load "Day5.fs"
+open Day5
+open System
+
+let instructions = input.Split([|"\n"|], StringSplitOptions.RemoveEmptyEntries)
+
+let rec processInstruction index length count (instructions:int []) =
+    if index >= length then count
+    else
+        let instruction = instructions.[index]
+        Array.set instructions index (instruction+1)
+        processInstruction (index+instruction) length (count+1) instructions
+
+instructions |> Array.map Int32.Parse |> processInstruction 0 (instructions |> Array.length) 0
+
+//[0; 3; 0; 1; -3] |> List.toArray |> processInstruction 0 5 //(instructions |> Array.length)
+
+
---
+++ AdventOfCode2017/Day5_PartTwo.fsx
@@ -0,0 +1,15 @@
+﻿#load "Day5.fs"
+open Day5
+open System
+
+let instructions = input.Split([|"\n"|], StringSplitOptions.RemoveEmptyEntries)
+
+let rec processInstruction index length count (instructions:int []) =
+    if index >= length then count
+    else
+        let instruction = instructions.[index]
+        let offset = if instruction >= 3 then -1 else 1
+        Array.set instructions index (instruction+offset)
+        processInstruction (index+instruction) length (count+1) instructions
+
+instructions |> Array.map Int32.Parse |> processInstruction 0 (instructions |> Array.length) 0
